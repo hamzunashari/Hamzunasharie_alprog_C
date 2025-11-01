896ca44fbade70a4a3ff71c3e6a8192627513c9a
hamzunashari <hamzunashari@gmail.com>
2025-11-01 23:06:16 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,39 @@
+# variabel adalah cara kita menyimpan nilai
+
+# memberikan nilai\assigment
+x=10
+y =6
+print(x)
+lebar = 50
+
+print ("nilai dari x =",x)
+print("nilai dari y =",y)
+print("nilai dari lebar" ,lebar)
+
+#penamaaan variabel
+jumlah_z = 9 # jika lebih dari satu kata ,maka hanya bisa di gabung dengan undercore
+bet99 = 100000 #tidak boleh diawali angka
+jumlahZ = 10
+
+# pemanggilan kedua
+print ("nilai dari x =",x)
+x = 99
+print ("nilai dari x =",x)
+
+
+# pemanggilan indirect
+a = x
+b =lebar
+print ("nilai dari a =", a)
+print ("nilai dari b =", b) 
+
+
+
+# nama saya =(hamzun asharie)
+# nim saya =(25241069)
+
+nama = "hamzun asharie"
+nim = 25241069
+
+print("nama saya adalah",nama)
+print("nim saya adalah",nim)
