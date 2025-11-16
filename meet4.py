e6ac1acd4593bbbb31f645909d3838c7db3bd4c9
hamzunashari <hamzunashari@gmail.com>
2025-11-16 13:40:49 +0700

    Add files via upload

---
+++ meet4.py
@@ -0,0 +1,40 @@
+#menggambil input dari user 
+
+nama = input ("masukan nama anda ")
+print("selamat datang", nama)
+print("nilai dari nama",nama)
+print(type(nama))
+
+bil_int =int(input("masukan bilangan anda "))
+print("bilangan anda adalah ", bil_int)
+print("nilai dari bil_int", bil_int)
+print(type(bil_int))
+
+bil_float = float(input("masukan bilangan anda"))
+print("bilangan anda adalah", bil_float)
+print("nilai dari bil_float", bil_float)
+print(type(bil_float))
+
+bil_bool =bool(input("masukan bilangan anda"))
+print("bilangan anda adalah", bil_bool)
+print("nilai dari bil_bool",bil_bool)
+print(type(bil_bool))
+
+# buatlah program dengan output sebagai berikut
+# masukan nama anda :
+# masukan umur anda :
+# nama anda adalah (nama)
+# tipe data dari nama adalah (type dari nama)
+# umur anda adalah (umur)
+# tipe data dari umur anda adlah (tipe dari umur
+
+nama = input ("masukan nama anda ")
+print("nama saya adalah",nama)
+print("nilai dari nama nama saya", nama)
+print(type(nama))
+
+bil_int = int(input("masukan umur anda"))
+print("umur anda adalah", bil_int)
+print("nilai dari umur", bil_int)
+print(type(bil_int))
+
