c3f457c63fe723dc6aac50d62184c503e5c355a0
hamzunashari <hamzunashari@gmail.com>
2025-11-28 20:22:23 +0700

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,58 @@
+# oprasi aritmatika (x : + -)
+# penjumlahan
+a = 17
+b= 3
+hasil = a + b
+print(a, "+",b, "=", hasil)
+
+# pengurangan -
+# perkalian *
+# pembagian /
+hasil = a / b
+print(a, "/", b, "=", hasil)
+# perpangkatan **
+
+# modulus = (sisa bagi)
+hasil = a % b
+print(a, "modulus",b,"=",hasil)
+
+# floor division //
+hasil = a // b
+print(a, "floor div",b,"=", hasil)
+
+# oprasi prioritas
+# 1. ()
+# 2. pangkat akar
+# 3. x :
+# 4. + -
+ 
+ # oprasi modulus a = 10
+a = 10
+b = 3
+hasil = a % b
+print(a, "%", b, "+", hasil)
+
+# oprasi floor division
+a = 10
+b = 3
+hasil = a // b
+print(a, "//", b, "=", hasil)
+
+# oprasi pengurangan
+a = 9
+b = 3
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# oprasi perkalian
+a = 9
+b = 3
+hasil = a * b 
+print(a, "*", b, "=", hasil)
+
+# oprasi perpangkatan 
+a = 2
+b = 3 
+hasil = a ** b 
+print(a, "**", b, "=", hasil)
+
