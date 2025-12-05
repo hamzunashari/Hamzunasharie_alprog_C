5f28161e1b3aad0a07ec1efb35b5b6d82673f6f1
hamzunashari <hamzunashari@gmail.com>
2025-12-05 15:19:11 +0700

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,20 @@
+# oprasi aritmatika
+# buatlah program konversi suhu
+# nama : hamzun asharie
+# nim : 25241069
+# masukan suhu anda dalam celcius
+# suhu anda dalam fahrenheit
+# masukan suhu anda dalam reamur
+# masukan suhu anda dalam kelvin
+      
+celcius = float(input("masukan suhu anda dalam celcius :"))
+fahren = 9/5 * celcius + 32
+print("suhu anda dalam fahrenheit :", fahren)
+
+celcius = float(input("masukan suhu anda dalam celcius :"))
+reamur = 4/5 * celcius
+print("suhu anda dalam reamur ;", reamur)
+
+celcius = float(input("masukan suhu anda dalam celcius :"))
+kelvin = celcius + 273
+print("suhu anda dalam kelvin", kelvin)
