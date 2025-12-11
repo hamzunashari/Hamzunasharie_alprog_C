45be00cca2ef0ee51d6ccce8b9a0277896a8cd77
hamzunashari <hamzunashari@gmail.com>
2025-12-11 21:12:06 +0700

    Add files via upload

---
+++ meet7.py
@@ -0,0 +1,28 @@
+# komparasi true  dan false
+# ><= <== != is is not 
+x = 25
+y = 69
+
+print("========== lebih dari (>)")
+print(x > y) 
+print(x, ">", y, "=", x > y) 
+
+print("========== kurang dari (<)") 
+print(x < y) 
+print(x, "<", y, "=", x < y) 
+
+print(">= lebih dari sama dengan") 
+print(x >= y) 
+print(x, ">=", y, "=", x >= y)
+
+print("<= kurang dari sama dengan")
+print(x <= y) 
+print(x, "<=", y, "=", x <= y)
+
+print("== sama dengan")
+print(x == y) 
+print(x, "==", y, "=", x == y)
+
+print("!= tidak sama dengan") 
+print(x != y) 
+print(x, "!=", y, "=" , x != y) 
