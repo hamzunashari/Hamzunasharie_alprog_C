94451b1168db46886a2bad667a98f2bdc38da658
hamzunashari <hamzunashari@gmail.com>
2026-04-12 15:07:27 +0700

    Add files via upload

---
+++ if end else.py
@@ -0,0 +1,9 @@
+nama = input("masukan nama anda :")
+if nama == "hamzun asharie":
+    print("nama saya hamzun asharie")
+    print("nim saya 25241069")
+    
+else:
+    print("nama anda salah/tidak benar")
+    print("nim anda salah")
+    print("program selesai")
